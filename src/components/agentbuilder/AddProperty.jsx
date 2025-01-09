d4b6aad8efdb834ce2cfcde59fe0971b9b306e59
shalini <s.rathore43@gmail.com>
2025-01-09 10:11:35 +0530

    ghv

--- src/components/agentbuilder/AddProperty.jsx
+++ src/components/agentbuilder/AddProperty.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useRef } from 'react'
 
 export default function AddProperty() {
+  let imgref = useRef("")
+  // http://localhost:8080/property
+  // const AddData=()=>
+  //   {   const config = {headers: {'Content-Type': 'multipart/form-data'}}
+
+     
+  //       const place ={name:nameref.current.value,resume:imgref.current.files[0]}
+  //       axios.post("http://localhost:8080/forposthandle",place,config,
+  //       ).then((d)=>console.log(d))
+  const saveProperty = ()=>
+  {
+    let data = {img:imgref.current.files[0]}
+    console.log(data)
+    const config = {headers: {'Content-Type': 'multipart/form-data'}}
+    axios.post("http://localhost:8080/property",data,config)
+    .then((d)=>console.log(d))
+    .catch((err)=>console.log(err))
+  }
   return (
-    <div>AddProperty</div>
+    <div>addproperty
+      <p>SElect propery Image:
+        <input type="file" ref={imgref} />
+      </p>
+      <input type="button" value="Save" onClick={()=>saveProperty()}/>
+</div>
+
   )
 }
